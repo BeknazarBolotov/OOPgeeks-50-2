a590d082ed6a9c5de29e4e27b5a2b2063b3aef3f
BeknazarBolotov <bolotov.beknazar08@gmail.com>
2025-01-13 23:48:36 +0600

    HW: hw3

--- HW/hw3.py
+++ HW/hw3.py
@@ -2,48 +2,60 @@ import random
 from abc import ABC, abstractmethod
 
 
-class Hero:
-    def __init__(self, name, lvl, hp, protection):
+class Hero(ABC):
+    def __init__(self, name, lvl, hp):
         self.name = name
         self.lvl = lvl
         self.hp = hp
-        self.__random_int = random.randint(0, 3)
-
+        self.__random_int = random.randint(1, 3)  # Приватный атрибут
 
     def attack(self):
-        return print(f"{self.name} атакует!")
+        print(f"{self.name} attacks the enemy!")
 
     def protection(self):
-        return print(f"{self.name} защищается")
+        print(f"{self.name} defends against the attack!")
 
     def rest(self):
         self.hp += 15
-        return print(f"{self.name} отдохнул и восполнил здоровье!")
+        print(f"{self.name} takes a rest to recover health. {self.hp}")
 
     def get_random_int(self):
-        pass
+        return self.__random_int  # Доступ к приватному атрибуту
 
     @abstractmethod
     def unique_attack(self):
-        return print(f"{self.name} выполняет сальто и наносит внезапный удар!")
+        pass
 
     @abstractmethod
     def unique_scream(self):
-        return print(f"{self.name} shouts: Is that all you've got? Let's dance!")
+        pass
 
     @abstractmethod
     def action(self):
-        action_type = self.get_random_int()
-        if action_type == 1:
-            self.attack()
-        elif action_type == 2:
-            self.protection()
-        elif action_type == 3:
-            self.rest()
-        else:
-            print("Unexpected action!")
+        pass
 
-
-from hw3 import Hero
 
 class Jester(Hero):
+        def unique_attack(self):
+            # Логика уникальной атаки: атака с неожиданным поворотом
+            print(f"{self.name} performs a tricky somersault and throws a surprise attack!")
+
+        def unique_scream(self):
+            print(f"{self.name} shouts: Is that all you've got? Let's dance!")
+
+        def action(self):
+            action_type = self.get_random_int()
+            if action_type == 1:
+                self.attack()
+            elif action_type == 2:
+                self.protection()
+            elif action_type == 3:
+                self.rest()
+            else:
+                print("Unexpected action!")
+
+hero = Jester("Jester", 100, 100)
+hero.unique_attack()
+hero.unique_scream()
+hero.action()
+hero.rest()
